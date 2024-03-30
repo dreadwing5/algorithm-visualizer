4623af18ada45ce020b30dcd8f81e40b5ca4cccf
dreadwing5 <sachinspyk@outlook.com>
2024-03-30 23:21:57 +0530

    Ready for DeployV1

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import ButtonBase from "@material-ui/core/ButtonBase";
+// import ButtonBase from "@material-ui/core/ButtonBase";
 
 import merge from "./merge.jpg";
 import selection from "./selection.jpg";
